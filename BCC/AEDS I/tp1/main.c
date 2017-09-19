cba43b84cf8f80284b8bf1f5eb9cb328aa77fda3
Ariel Santos <as-ariel@outlook.com>
2017-09-19 00:47:57 -0300

    Docs

--- BCC/AEDS I/tp1/main.c
+++ BCC/AEDS I/tp1/main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
+//Só to usando pq é FIFO e prático de guardar output
 FILE *output;
 
+//Processa cada conjunto de depósitos
 void depositos(int iter, int n)
 {
     int j = 0, z = 0, i = n - 1;
-    fprintf(output, "Teste %d\n", iter+1);
+    fprintf(output, "Teste %d\n", iter + 1);
     for (; n > 0; n--)
     {
-        int dj, dz;
+        int dj, dz; // depósito da rodada
         scanf("%d %d", &dj, &dz);
         if (dj < 0 || dz < 0)
             fprintf(output, "Vovo Vitoria nao tira dinheiro dos cofrinhos.\n");
@@ -37,6 +39,8 @@ void main()
             depositos(iter, linhas);
         }
     }
+
+    // Volta ao início do stream de output e printa em chuncks de 50 char
     rewind(output);
     char buffer[100];
     while (!feof(output))
