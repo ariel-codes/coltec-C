99cdf551d2d22f4bcd97e6b405d6771a4a54a875
Ariel Santos <as-ariel@outlook.com>
2018-03-21 20:54:09 -0300

    Separei tudo referente ao bacharelado em outra pasta

--- BCC/[unsorted]/naive-matrices/main.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "ui.h"
-
-int main(int argc, char* argv[]){
-    return usemode(argc, argv);
-}
--- BCC/[unsorted]/naive-matrices/ui.h
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-int usemode(int argc, char* argv[]);
-
